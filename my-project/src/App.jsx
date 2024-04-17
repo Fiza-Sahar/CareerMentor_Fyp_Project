17919b094f612ca7ed489367928416371206e167
Fiza Sahar <fizasahar66@gmail.com>
2024-04-17 22:11:11 +0500

    do some changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/AboutUs/About";
 import Features from "./components/Features/Features";
 import Login from "./components/LoginPage/Login";
-import Signup from "./components/SignupPage/Signup"
+import Signup from "./components/SignupPage/Signup";
+import Ourteam from "./components/OurTeam/Ourteam";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useRef, useState } from "react"; 
 
 const App = () => {
-  const aboutUsRef = useRef(null); 
+  const aboutUsRef = useRef(null);
+  const ourTeamRef = useRef(null);
 
   const handleAboutClick = () => {
-    aboutUsRef.current.scrollIntoView({ behavior: "smooth" }); 
+    aboutUsRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleOurTeamClick = () => {
+    ourTeamRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <Router>
-      <Navbar handleAboutClick={handleAboutClick} />
+      <Navbar handleAboutClick={handleAboutClick} handleOurTeamClick={handleOurTeamClick} />
 
       <Routes>
-        <Route path="/" element={<Home aboutUsRef={aboutUsRef} />} />
-        <Route path="/About" element={<About />} />
-
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About ref={aboutUsRef}/>} />
+        <Route path="/Ourteam" element={<Ourteam ref={ourTeamRef} />} />
         <Route path="/Features" element={<Features />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<Login />} />
-        {/* Add more routes for Team Contactus page*/}
       </Routes>
     </Router>
   );
